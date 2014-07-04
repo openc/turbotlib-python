6866e6b90fa470db72440f42e0451d7080dda556
Seb Bacon <seb.bacon@gmail.com>
2014-07-04 16:26:58 +0100

    Set up data/ directory as side effect of accessing it

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='turbotlib',
-    version='0.0.2',
+    version='0.0.3',
     description='Helpers for writing turbot bots.',
     packages=['turbotlib'],
     license='License :: OSI Approved :: MIT License',
--- turbotlib/__init__.py
+++ turbotlib/__init__.py
@@ -9,23 +9,20 @@ import yaml
 # We are running in production if MORPH_URL is set in the environment.
 in_production = bool(os.getenv('MORPH_URL'))
 
-if in_production:
-    data_dir = '/data'
-else:
-    data_dir = 'data'
 
-_vars_path = data_dir + '/_vars.yml'
+def data_dir():
+    if in_production:
+        data_dir = '/data'
+    else:
+        data_dir = 'data'
+        _set_up_data_dir(data_dir)
+    return data_dir
 
+def _vars_path():
+    return data_dir() + '/_vars.yml'
 
 def log(message):
     print(message, file=sys.stderr)
-
-
-def set_up_data_dir():
-    try:
-        os.mkdir(data_dir)
-    except OSError:
-        pass
 
 
 def save_var(key, val):
@@ -42,17 +39,19 @@ def get_var(key):
 
 
 def _save_vars(vars):
-    set_up_data_dir()
-
-    with open(_vars_path, 'w') as f:
+    with open(_vars_path(), 'w') as f:
         f.write(yaml.dump(vars))
 
 
 def _get_vars():
-    set_up_data_dir()
-
     try:
-        with open(_vars_path) as f:
+        with open(_vars_path()) as f:
             return yaml.load(f)
     except IOError:
         return {}
+
+def _set_up_data_dir(d):
+    try:
+        os.mkdir(d)
+    except OSError:
+        pass
